f339061668cff8454bd1c892ebd86cffb8321bc8
Mariyan <marian.sokolov1@gmail.com>
2023-10-03 17:24:03 +0300

    reserve product made

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -13,7 +13,7 @@ class ProductController {
       productsQuerySnapshot.forEach((productDoc) => {
         const product = productDoc.data();
         const productId = productDoc.id;
-        productsData.push({ productId, ...product });
+        productsData.push({ id: productId, ...product });
       });
 
       res.status(200).json(productsData);
@@ -62,12 +62,107 @@ class ProductController {
         const updatedOffers = userData?.offers ? [...userData.offers, productId] : [productId];
         await userRef.update({ offers: updatedOffers });
       }
-      res.status(201).json({ productId });
+      res.status(201).json({ id: productId });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   }
+
+  public async reserveProduct(req: Request, res: Response): Promise<void> {
+    try {
+      const { userId, productId, orderQuantity, minimumOrder, reservedCost } = req.body;
+  
+      // Fetch the product to check availability
+      const productRef = admin.firestore().collection('products').doc(productId);
+      const productDoc = await productRef.get();
+  
+      if (!productDoc.exists) {
+        res.status(404).json({ error: 'Продуктът не е намерен!' });
+        return;
+      }
+  
+      const productData = productDoc.data();
+  
+      if (!productData || productData.reserved) {
+        res.status(400).json({ error: 'Продуктът не е достъпен за резервация!' });
+        return;
+      }
+  
+      const currentAvailability = productData.availability;
+  
+      // Update the product's availability and mark it as reserved
+      const updatedAvailability = currentAvailability - orderQuantity;
+      await productRef.update({
+        availability: 0,
+        reserved: true,
+        reservedCost: parseFloat(reservedCost),
+      });
+  
+      // Fetch the updated product data
+      const updatedProductSnapshot = await productRef.get();
+      const updatedProductData = updatedProductSnapshot.data();
+  
+      // Update the user's reserved products
+      const userRef = admin.firestore().collection('users').doc(userId);
+      const userDoc = await userRef.get();
+  
+      if (!userDoc.exists) {
+        res.status(404).json({ error: 'Потребителят не е намерен!' });
+        return;
+      }
+  
+      const userData = userDoc.data();
+      const updatedReservedProducts = userData?.reservedProducts ? [...userData.reservedProducts, productId] : [productId];
+  
+      await userRef.update({ reservedProducts: updatedReservedProducts });
+  
+      // Fetch the updated user data
+      const updatedUserSnapshot = await userRef.get();
+      const updatedUserData = updatedUserSnapshot.data();
+  
+      console.log('updatedProductData', updatedProductData);
+      console.log('updatedUserData', updatedUserData);
+  
+
+      console.log('updatedAvailability', updatedAvailability);
+      console.log('minimumOrder', minimumOrder);
+      // if the new availability is more than the minimum order, we create a new product that can be reserved by the users
+      if (updatedAvailability > minimumOrder) {
+        const newProductId = admin.firestore().collection('products').doc().id;
+
+        const currentDate = new Date();
+        const formattedDate = `${currentDate.getDate()}.${currentDate.toLocaleString('default', { month: 'short' })}`;
+
+        const newProductData = {
+          ...productData,
+          id: newProductId,
+          reserved: false,
+          availability: updatedAvailability,
+          dateAdded: formattedDate,
+        };
+        await admin.firestore().collection('products').doc(newProductId).set(newProductData);
+        console.log('newProductData', newProductData);
+        res.status(200).json({
+          message: 'Продуктът е резервиран успешно и създаден нов продукт!',
+          updatedProduct: updatedProductData,
+          updatedUser: updatedUserData,
+          newProduct: newProductData,
+        });
+      } else {
+        res.status(200).json({
+          message: 'Продуктът е резервиран успешно!',
+          updatedProduct: updatedProductData,
+          updatedUser: updatedUserData,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+  
+  
 }
 
 export default new ProductController();
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/getAllProducts', productController.getAllProducts);
 router.post('/addProduct', productController.addProduct);
+router.post('/reserveProduct', productController.reserveProduct);
 
 export default router;
