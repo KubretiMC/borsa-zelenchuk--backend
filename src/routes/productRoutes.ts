dabb11f02492c12dbb7ff034d793fb1e80c8b01d
Mariyan <marian.sokolov1@gmail.com>
2023-10-13 00:17:00 +0300

    jwt autorization made

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import productController from '../controllers/productController';
+import { verifyToken } from '../utils/utils';
 
 const router = express.Router();
 
 router.get('/getAllProducts', productController.getAllProducts);
-router.post('/addProduct', productController.addProduct);
+router.post('/addProduct', verifyToken, productController.addProduct);
 router.post('/reserveProduct', productController.reserveProduct);
 router.post('/finishProduct', productController.finishProduct);
 
---
+++ src/utils/utils.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export function verifyToken(req: Request, res: Response, next: NextFunction) {
+  const token = req?.headers?.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(403).send({ auth: false, message: 'No token provided.' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY as string;
+  jwt.verify(token, secretKey, (err, decoded: any) => {
+    if (err) {
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    }
+    req.userId = decoded.userId;
+    next();
+  });
+}
